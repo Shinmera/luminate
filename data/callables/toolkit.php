578e1ae7daa0c623066a34a4581f1aeb8e94c8e2
Shinmera <shinmera@tymoon.eu>
2012-04-19 11:33:28 +0200

    Added timeAgo function to calculate elapsed time.

--- data/callables/toolkit.php
+++ data/callables/toolkit.php
@@ -354,6 +354,26 @@ function toDate($time){
         return date('l d.m.Y H:i:s',strtotime($time));
 }
 
+function timeAgo($time){
+    $periods = array("second", "minute", "hour", "day", "week", "month", "year", "decade");
+    $lengths = array("60","60","24","7","4.35","12","10");
+
+    $now = time();
+    $difference = $now - $time;
+
+    for($j = 0; $difference >= $lengths[$j] && $j < count($lengths)-1; $j++) {
+        $difference /= $lengths[$j];
+    }
+
+    $difference = round($difference);
+
+    if($difference != 1) {
+        $periods[$j].= "s";
+    }
+
+    return $difference.' '.$periods[$j].' ago';
+}
+
 function convertHTML($html){
     $html = str_replace("<","&lt;",$html);
     $html = str_replace(">","&gt;",$html);
