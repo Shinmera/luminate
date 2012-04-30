b399fb9fd50fb1256506648f3676a6d9fd1312da
shinmera <shinmera@tymoon.eu>
2012-04-30 14:49:11 +0200

    Continued work on unnecessary selectData function.

--- data/callables/DataModel.php
+++ data/callables/DataModel.php
@@ -63,46 +63,126 @@ class DataModel{
         $c->query($query,$data);
     }
     
-    //@untested
-    //TODO: Add opertors for selected values.
     //TODO: Somehow allow more complex operations on selects and wheres: ( WHERE x/2=y )
-    //TODO: Somehow allow more complex operations on selects and wheres with multiple function arguments, ( CONCAT(x,y) = z )
-    public static function selectData($table,$fields,$whereString=null,$whereVars=array(),$limit=null,$innerJoinTable=null,$innerJoinVars=null,$loadFields=false){
+    //TODO: Add support for multi-parameter function calls
+    /**
+     * Arguments explained in proper structure:
+     * ( 
+     *   :table table
+     *   :fields ( &body field1 field2 )
+     *           ( &body (:field &opt :table=&:table :function='' :args='' :as=&:field )
+     *   &opt
+     *   :jointable table=NIL
+     *   :joinvars field=NIL
+     *             ( field &opt field2 )
+     *   :joincomparison comparison=NIL
+     *   :where wherestruct
+     *   :vars ( &body ( key1 value1 &opt comparison='=' keytable=&:table valuetable=NIL keyoperator='' valueoperator='' )
+     *   :order order=NIL
+     *          ( field )
+     *          ( &body ( field &opt direction='ASC' table=&:table ) )
+     *   :limit limit=NIL
+     *          ( limit )
+     *          ( lower offset )
+     * )
+     * 
+     * Simple function call example:
+     * MIN  "SELECT username,relationship FROM users WHERE awesomeness=100"
+     * CALL selectData(array(
+     *                  "table"=>"users",
+     *                  "fields"=>array("username","relationship"),
+     *                  "where"=>"?","vars"=>array(array("awesomeness",100))));
+     * RES  "SELECT `users`.`username` AS 'username',`users`.`relationship` AS 'relationship' FROM `users` WHERE `users`.`awesomeness` = 100"
+     * 
+     * Complex function call example:
+     * TO   selectData(array("table"=>"users",
+     *                       "fields"=>array("id",
+     *                          array("field"=>"id","function"=>"COUNT","as"=>"COUNT"),
+     *                          array("field"=>"tel","table"=>"telephone")),
+     *                       "jointable" => "telephone",
+     *                       "joinvars"  => array("id","uid"),
+     *                       "where"     => "(?|?|?)&?",
+     *                       "vars"      => array(
+     *                                           array( "username" "Mithent"),
+     *                                           array( "username "Shinmera" "LIKE"),
+     *                                           array( "username" "%A%" "LIKE"),
+     *                                           array( "awesomeness" 90 ">")
+     *                                      ),
+     *                       "order"     => array(
+     *                                           "firstname"
+     *                                           "lastname"
+     *                                           array("awesomeness" "DESC")
+     *                                      ),
+     *                       "limit"     => 20));
+     * RES  "SELECT `users`.`id` AS 'id',COUNT(`users`.`id`) AS 'COUNT',`telephone`.`tel` AS 'tel'
+     *       FROM `users` INNER JOIN `telephone` ON `users`.`id`=`telephone`.`uid`
+     *       WHERE ( `users`.`username` = 'Mithent' OR `users`.`username` LIKE 'Shinmera' OR `users`.`username` LIKE '%A%' ) AND `users`.`awesomeness` > 90
+     *       ORDER BY `users`.`firstname` ASC,`users`.`lastname` ASC,`users`.`awesomeness` DESC 
+     *       LIMIT 20"
+     */
+    public static function selectData($args){
+        global $k;$data=array();
+        
+        $table=$args['table'];
+        $fields=$args['fields'];
+        
+        //Validate
         if(!is_string($table)||$table=="")throw new Exception('No valid table name specified.');
         if(!is_array($fields))throw new Exception('$fields has to be an array!');
         if(count($fields)==0)throw new Exception('No field to select specified.');
-        global $k;$data=array();
+        if(!is_array($args))throw new Exception('Args needs to be an array!');
         
-        if(!$k->isAssociative($fields))
-            $query = 'SELECT `'.$table.'`.`'.implode('`,`'.$table.'`.`',$fields).'` FROM `'.$table.'` ';
-        else{
-            $query = 'SELECT ';
-            foreach($fields as $field => $ftable){
-                $query.= '`'.$ftable.'`.`'.$field.'`,';
+        //Defaults
+        if(!isset($args['where']))$whereString=null;        else $whereString   =$args['where'];
+        if(!isset($args['vars'])) $whereVars  =array();     else $whereVars     =$args['vars'];
+        if(!isset($args['limit']))$limit      =null;        else $limit         =$args['limit'];
+        if(!isset($args['order']))$orders     =null;        else $orders        =$args['order'];
+        if(!isset($args['jointable']))$innerJoinTable=null; else $innerJoinTable=$args['jointable'];
+        if(!isset($args['joinvars'])) $innerJoinVars =null; else $innerJoinVars =$args['joinvars'];
+        if(!isset($args['joincomparison']))$innerJoinComparison='=';else $innerJoinComparison=$args['joincomparison'];
+        
+        //Compose SELECT part
+        $query = 'SELECT ';
+        foreach($fields as $field){
+            if(!is_array($field))
+                $query.= '`'.$table.'`.`'.$field.'` AS \''.$field.'\'';
+            else{
+                if(!isset($field['table']))$field['table']=$table;
+                if(isset($field['function']))$field['function'].='(';
+                if(!isset($field['as']))$field['as']=$field['field'];
+
+                $query.= $field['function'].'`'.$field['table'].'`.`'.$field['field'].'`';
+                if(isset($field['args']))$query.=','.implode(',',$field['args']);
+                if(isset($field['function']))$query.=')';
+                $query.= ' AS \''.$field['as'].'\'';
             }
-            $query= substr($query,0,strlen($query)-1).' FROM `'.$table.'` ';
+            $query.=',';
         }
+        $query=substr($query,0,strlen($query)-1).' FROM `'.$table.'` ';
         
+        //Compose INNER JOIN part
         if($innerJoinTable!=""){
             $query.='INNER JOIN `'.$innerJoinTable.'` ';
             if(!is_array($innerJoinVars))$query.='USING(`'.$innerJoinVars.'`) ';
             else{
+                
                 switch(count($innerJoinVars)){
                     case 0:throw new Exception("InnerJoinVars cannot be empty!");break;
                     case 1:$query.='USING(`'.$innerJoinVars[0].'`) ';break;
-                    case 2:$query.='ON `'.$table.'`.`'.$innerJoinVars[0].'` = `'.$innerJoinTable.'`.`'.$innerJoinVars[0].'` ';break;
+                    case 2:$query.='ON `'.$table.'`.`'.$innerJoinVars[0].'` '.$innerJoinComparison.' `'.$innerJoinTable.'`.`'.$innerJoinVars[1].'` ';break;
                     default:throw new Exception("InnerJoinVars does not accept more than 2 arguments!");break;
                 }
             }
         }
         
+        //Compose WHERE part
         if(count($whereVars)>0){
             if(!is_array($whereVars)&&$whereVars!=null)
                 throw new Exception("Unexpected where variable format!\nWhereVars: ".print_r($whereVars, true));
             if(count($whereVars)!=substr_count($whereString,"?"))
                 throw new Exception("Unmatched where variables!\nWhereString: ".$whereString."\nWhereVars: ".print_r($whereVars,true));
             
-            //Where vars: :key ( :value &opt :conjunction :table1 :table2 :operator1 :operator2)
+            //Where vars: ( :key :value &opt :conjunction :table1 :table2 :operator1 :operator2 )
             //Defaults:     ?    ?           =            null    null    ''         '' 
             $query.='WHERE ';
             $whereString=str_replace('|',' OR ',str_replace('&',' AND ',$whereString));
@@ -114,7 +194,7 @@ class DataModel{
                     throw new Exception("Elements of WhereVars have to be an array!\nWhereString: ".$whereString.'\nWhereVars: '.print_r($whereVars,true));
                 }else{
                     switch(count($els)){
-                        case 0:throw new Exception("Unexpected empty where value!\nWhereString: ".$whereString.'\nWhereVars: '.print_r($whereVars,true));break;
+                        case 0:throw new Exception("Unexpected empty where key!\nWhereString: ".$whereString.'\nWhereVars: '.print_r($whereVars,true));break;
                         case 1:throw new Exception("Value is missing!\nWhereString: ".$whereString.'\nWhereVars: '.print_r($whereVars,true));break;
                         case 2:$els[2]='=';
                         case 3:$els[3]=null;
@@ -151,6 +231,27 @@ class DataModel{
             $query.=$where[$i].' ';
         }
         
+        //Compose ORDER BY part
+        if($orders!=null){
+            $query.='ORDER BY ';
+            if(!is_array($orders))$query.='`'.$table.'`.`'.$orders.'` ASC ';
+            else{
+                foreach($orders as $order){
+                    if(!is_array($order))$query.='`'.$table.'`.`'.$order.'` ASC,';
+                    else{
+                        switch(count($order)){
+                            case 0:break;
+                            case 1:$order[1]='ASC';
+                            case 2:$order[2]=$table;
+                            case 3:$query.='`'.$order[2].'`.`'.$order[0].'` '.$order[1].',';break;
+                        }
+                    }
+                }
+                $query=substr($query,0,strlen($query)-1).' ';
+            }
+        }
+        
+        //Compose LIMIT part
         if($limit!=null){
             if(!is_array($limit))$query.='LIMIT '.$limit;
             else{
@@ -159,10 +260,10 @@ class DataModel{
             }
         }
         
+        //Execute query
         return DataModel::getData($table, $query, $data, array(), $loadFields);
     }
     
-    //@deprecated
     public static function getData($table,$query,$args=array(),$fields=array(),$loadFields=false){
         global $c;
         if($loadFields)$fields=$c->getTableColumns($this->table);
@@ -176,10 +277,10 @@ class DataModel{
         else return null;
     }
     
-    public static function getHull($table){
+    public static function getHull($table,$loadFields=false){
         global $c;
-        //$fields = $c->getData('SELECT column_name AS name FROM information_schema.columns WHERE table_name=? AND table_schema=?',array($table,SQLDB));
-        return new DataModel($table);
+        if($loadFields)$fields=$c->getTableColumns($this->table);
+        return new DataModel($table,$fields);
     }
 }
 ?>
