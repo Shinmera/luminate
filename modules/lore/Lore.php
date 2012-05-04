4dbf42c5bf6fbbe5b0dc1d358781ed770de107cb
shinmera <shinmera@tymoon.eu>
2012-05-05 00:50:59 +0200

    Fixed cache path. Modified for new articles system.

--- modules/lore/Lore.php
+++ modules/lore/Lore.php
@@ -23,17 +23,17 @@ function displayAdminPage(){
 
 function displayPage(){
     global $t,$k,$params,$SUPERIORPATH,$MODULECACHE,$page,$type,$action;
-    include(MODULEPATH.$MODULECACHE['Lore_Article']);
+    define('CACHEPATH',ROOT.DATAPATH.'cache/lore/');
     
     $params[0]=str_replace('_',' ',$k->sanitizeString($params[0],'\s\-_'));
     $params[1]=str_replace('_',' ',$k->sanitizeString($params[1],'\s\-_'));
-    switch($params[0]){
+    switch(strtolower($params[0])){
         case 'category':
         case 'portal':
         case 'special':
         case 'file':
         case 'user':
-            $type=$params[0];
+            $type=strtolower($params[0]);
             $page=$params[1];
             $params=array_slice($params, 1);
             break;
@@ -49,17 +49,26 @@ function displayPage(){
     else                      $revision=-1;
     if(!in_array($params[1],array('edit','history','discuss')))$action='view';
     else                                                       $action=$params[1];
+    if(strlen($page)>127)$page=substr($page,0,127);
     
     $t->loadTheme("lore");
-    $t->openPage($params[0].' - Lore');
-    
-    
-    if($type!='article')include(MODULEPATH.$MODULECACHE['Lore_'.ucfirst($type)]);
-    $temp = ucfirst($type);
-    $object = new $temp($page,$revision.ucfirst($action));
-    call_user_func(array(&$object,'display'.ucfirst($action)));
-    
-    $t->closePage();
+    switch($type){
+        case 'special':
+            include(MODULEPATH.$MODULECACHE['Lore_Special']);
+            $special = new Special();
+            $special->display($page);
+            break;
+        case 'file':
+            include(MODULEPATH.$MODULECACHE['Lore_File']);
+            $file = new File($page,$revision);
+            call_user_func(array(&$file,'display'.ucfirst($action)));
+            break;
+        default:
+            include(MODULEPATH.$MODULECACHE['Lore_Article']);
+            $article = new Article($page,$type,$revision);
+            call_user_func(array(&$article,'display'.ucfirst($action)));
+            break;
+    }
 }
 
 
