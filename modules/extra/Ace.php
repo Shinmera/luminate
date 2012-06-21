07e1ae437d49ed9961fcaff48350f8a2eb370407
shinmera <shinmera@tymoon.eu>
2012-06-21 22:49:36 +0200

    added api function.

--- modules/extra/Ace.php
+++ modules/extra/Ace.php
@@ -6,6 +6,14 @@ public static $short='ace';
 public static $required=array();
 public static $hooks=array("foo");
 
+function displayAceForFile($file){
+    if(substr($file,0,7)!='http://')die();
+    
+    $filetype=substr($file,strrpos($file, ".")+1);
+    if($filetype=="js")$filetype="javascript"; //All others match file ending and type already.
+    $this->getAceEditor("source",$filetype,file_get_contents($file),"width:100%;height:500px;");
+}
+
 function getAceEditor($formname,$mode,$content="",$style=""){
     ?>
     <div name="contents" id="editor" style="<?=$style?>"><?=htmlspecialchars($content)?></div>
