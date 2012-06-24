8d3facd2a1574f674700ca4d7e0636fc3fcd9ffc
shinmera <shinmera@tymoon.eu>
2012-06-24 16:46:10 +0200

    Added functions to parse DataModel instances to XML,JSON,BLOCK or LISP format.

--- modules/core/Pipe.php
+++ modules/core/Pipe.php
@@ -6,27 +6,77 @@ public static $short='pipe';
 public static $required=array();
 public static $hooks=array();
 
-private $types = array('xml','json','block');
+private $types = array('XML','JSON','BLOCK','LISP');
 
-function parseDataModel($datamodel,$type){
+function parseDataModel($datamodel,$type="XML"){
     if(!$this->isTypeSupported($type))throw new Exception('Requested format type is not supported!');
     return call_user_func(array(&$this,'parseTo'.strtoupper($type)), $datamodel);
 }
 
 function isTypeSupported($type){
-    return in_array($type,$this->types);
+    return in_array(strtoupper($type),$this->types);
 }
 
 function parseToXML($data){
-    
+    $res='';
+    foreach($data as $dat){
+        $res.='<row table="'.$dat->getTable().'">';
+        
+        $fields = $dat->getHolder();
+        foreach($fields as $key => $value){
+            $res='<field name="'.$key.'" value="'.$value.'" />';
+        }
+        
+        $res.='</row>';
+    }
+    return $res;
 }
 
 function parseToJSON($data){
-    
+    $rows = array();
+    foreach($data as $dat){
+        $inner = array('"table":"'.$dat->getTable().'"');
+        
+        $fields = $dat->getHolder();
+        foreach($fields as $key => $value){
+            if(is_numeric($value))  $inner[]='"'.$key.'": '.$value;
+            else                    $inner[]='"'.$key.'":"'.$value.'"';
+        }
+        
+        $rows[]='{'.implode(',',$inner).'}';
+    }
+    return '['.implode(',',$rows).']';
 }
 
 function parseToBLOCK($data){
-    
+    $res='';
+    foreach($data as $dat){
+        $res.=$dat->getTable()."{\n";
+        
+        $fields = $dat->getHolder();
+        foreach($fields as $key => $value){
+            $res=$key.':'.$value."\n";
+        }
+        
+        $res.="}\n";
+    }
+    return $res;
+}
+
+function parseToLISP($data){
+    $rows = array();
+    foreach($data as $dat){
+        $inner = array('(:table '.$dat->getTable().')');
+        
+        $fields = $dat->getHolder();
+        foreach($fields as $key => $value){
+            if(is_numeric($value))  $inner[]='(:'.$key.' '.$value.')';
+            else                    $inner[]='(:'.$key.' "'.$value.'")';
+        }
+        
+        $rows[]='('.implode(' ',$inner).')';
+    }
+    return '('.implode(' ',$rows).')';
 }
 
 
