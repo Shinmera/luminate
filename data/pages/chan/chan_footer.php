e4df19c5f906d25ac3adf621014e9884ab5fb550
shinmera <shinmera@tymoon.eu>
2012-06-26 19:25:15 +0200

    Updated to use direct inlining.

--- data/pages/chan/chan_footer.php
+++ data/pages/chan/chan_footer.php
@@ -1,52 +1,63 @@
 <? function write_footer($title,$board,$boardfolder,$thread=0,$options=""){
     global $c,$k,$a,$t;
-    $write ='<div id="threadWatch" class="threadWatch" style="display:none;float:left;position:absolute;">';
-        $write.='<table><thead>';
-            $write.='<tr><th id="watchRemoveCol"></th><th id="watchBoardCol">Board</th><th id="watchIDCol">ID</th>
-                     <th id="watchPosterCol">Poster</th><th id="watchTitleCol">Title</th><th id="watchUnreadCol">Unread</th></tr>';
-        $write.='</thead><tbody>';
-        $write.='</tbody></table>';
-        $write.='<a href="#" id="watchRefreshButton" class="watchButton" title="Refresh">↻</a> ';
-        $write.='<a href="#" id="watchReadButton" class="watchButton" title="Read All">✔</a> ';
-        $write.='<a href="#" id="watchClearButton" class="watchButton" title="Clear">✘</a> ';
-    $write.='</div>';
-    $write.='<div id="popup" class="popup" style="display:none;">Please wait...</div>';
-    $write.='<div id="previewPost" style="display:none;float:left;position:absolute;"></div>';
-    $write.='<img id="previewImage" style="display:none;max-width:400px;max-height:400px;float:left;position:absolute;" alt="preview"/>'."\n";
-    //REPORT AND DELETE SHIT
-    $write.='<div class="deleteBox">'."\n";
-    $write.='<input type="hidden" name="varboard" value="'.$board.'" />'."\n";
-    $write.='<input type="hidden" name="varfolder" id="varfolder" value="'.$boardfolder.'" />'."\n";
-    $write.='<label>Delete:</label><input type="checkbox" name="varfileonly" value="1" /> File only<br />';
-    $write.='<input type="password" name="varpassword" class="password" />'."\n";
-    $write.='<input type="submit" name="submitter" value="Delete" />'."\n";
-    $write.='</div><div class="reportBox">'."\n";
-    $write.='<label>Report:</label><br /><input type="text" name="varreason" placeholder="reason"/>'."\n";
-    $write.='<input type="submit" name="submitter" value="Report" />'."\n";
-    $write.='</div></form>'."\n";
+    
+    ?>
+    <div id="threadWatch" class="threadWatch" style="display:none;float:left;position:absolute;">
+        <table><thead>
+            <tr>
+                <th id="watchRemoveCol"></th>
+                <th id="watchBoardCol">Board</th>
+                <th id="watchIDCol">ID</th>
+                <th id="watchPosterCol">Poster</th>
+                <th id="watchTitleCol">Title</th>
+                <th id="watchUnreadCol">Unread</th>
+            </tr>
+        </thead><tbody>
+        </tbody></table>
+        <a href="#" id="watchRefreshButton" class="watchButton" title="Refresh">↻</a> 
+        <a href="#" id="watchReadButton" class="watchButton" title="Read All">✔</a> 
+        <a href="#" id="watchClearButton" class="watchButton" title="Clear">✘</a> 
+    </div>
+    <div id="popup" class="popup" style="display:none;">Please wait...</div>
+    <div id="previewPost" style="display:none;float:left;position:absolute;"></div>
+    <img id="previewImage" style="display:none;max-width:400px;max-height:400px;float:left;position:absolute;" alt="preview"/>
+    
+    <div class="deleteBox">
+    <input type="hidden" name="varboard" value="<?=$board?>" />
+    <input type="hidden" name="varfolder" id="varfolder" value="<?=$boardfolder?>" />
+    <label>Delete:</label><input type="checkbox" name="varfileonly" value="1" /> File only<br />
+    <input type="password" name="varpassword" class="password" />
+    <input type="submit" name="submitter" value="Delete" />
+    </div><div class="reportBox">
+    <label>Report:</label><br /><input type="text" name="varreason" placeholder="reason"/>
+    <input type="submit" name="submitter" value="Report" />
+    </div></form>
 
-    $write.='</div><br clear="all">'."\n";
+    </div><br clear="all">
 
-    //FOOTER CONTENT
-    $write.='<link rel="alternate" type="application/rss+xml" title="'.$boardfolder.' RSS feed" href="http://'.HOST.PROOT.'api.php?m=Chan&c=getBoardRSS&a=board:'.$board.'" />';
-    $write.='<script type="text/javascript">var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");'."\n";
-    $write.='document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));'."\n";
-    $write.='</script><script type="text/javascript">'."\n";
-    $write.='try {var pageTracker = _gat._getTracker("UA-13229468-2");pageTracker._trackPageview();} catch(err) {}</script>'."\n";
-    $write.='<script type="text/javascript">var boardID="'.$board.'";var boardName="'.$boardfolder.'";</script>'."\n";
+    <link rel="alternate" type="application/rss+xml" title="<?=$boardfolder?> RSS feed" href="http://<?=HOST.PROOT?>api.php?m=Chan&c=getBoardRSS&a=board:<?=$board?>" />
+    <script type="text/javascript">var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+    document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));
+    </script><script type="text/javascript">
+    try {var pageTracker = _gat._getTracker("UA-13229468-2");pageTracker._trackPageview();} catch(err) {}</script>
+    <script type="text/javascript">var boardID="<?=$board?>";var boardName="<?=$boardfolder?>";</script>
 
-    $write.='<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js" ></script>'."\n";
-    $write.='<script type="text/javascript" src="'.PROOT.'callables/plugins.js" ></script>'."\n";
-    $write.='<script type="text/javascript" src="'.PROOT.'callables/js.js" ></script>'."\n";
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js" ></script>
+    <script type="text/javascript" src="<?=PROOT?>callables/plugins.js" ></script>
+    <script type="text/javascript" src="<?=PROOT?>callables/js.js" ></script>
 
-    //ACTUAL FOOTER
-    global $GEN_STARTTIME;
-    $time = explode(' ',microtime());$time = $time[1]+$time[0];$total_time = round(($time-$GEN_STARTTIME),4);
-    $write.='<? $time = explode(" ",microtime());$time = $time[1]+$time[0];$total_time = round(($time-STARTTIME),4); ?>';
-    $write.='<div class="footer">&copy;2010-<?=date("Y")?> TymoonNET/NexT <br />'."\n";
-    $write.='Static/Dynamic page generated in '.$total_time.'/<?=$total_time?> seconds.<br />'."\n";
-    $write.='Running TyNET v'.VERSION.'</div>'."\n";
-    $write.='</body>'."\n";
-    $write.='</html><? ob_end_flush(); ?>'."\n";
-    return $write;
+    <? global $GEN_STARTTIME;
+    $time = explode(' ',microtime());$time = $time[1]+$time[0];$total_time = round(($time-$GEN_STARTTIME),4); ?>
+    <?='<? $time = explode(" ",microtime());$time = $time[1]+$time[0];$total_time = round(($time-STARTTIME),4); ?>'?>
+    <div class="footer">
+        &copy;2010-<?=date("Y")?> TymoonNET/NexT <br />
+        Static/Dynamic page generated in <?=$total_time?>/<?='<?=$total_time?>'?> seconds.<br />
+        Running TyNET v<?=VERSION?>
+    </div>
+    </body>
+    </html>
+    <?='<? ob_end_flush(); ?>'?>
+    
+    <?
+    
 } ?>
