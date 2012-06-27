bc4a0275455b2d834a5475b65251520d012e9e1b
shinmera <shinmera@tymoon.eu>
2012-06-27 10:16:22 +0200

    Updated directory and secured against no banners.

--- data/pages/chan/chan_glob_header.php
+++ data/pages/chan/chan_glob_header.php
@@ -1,7 +1,10 @@
-<? $folder = opendir(ROOT.IMAGEPATH.'banners/');
+<? $folder = opendir(ROOT.IMAGEPATH.'chan/headers/');
+$banners = array();
 while(($file=readdir($folder))!==FALSE){
         if($file!="."&&$file!="..")$banners[]=$file;
 }
-$banner=$banners[mt_rand(0,count($banners)-1)];
-echo("<center><img src='".IMAGEPATH.'banners/'.$banner."' alt='banner' id='pageBanner' /></center>");
+if(count($banners)>0){
+    $banner=$banners[mt_rand(0,count($banners)-1)];
+    echo("<center><img src='".IMAGEPATH.'banners/'.$banner."' alt='banner' id='pageBanner' /></center>");
+}
 ?>
