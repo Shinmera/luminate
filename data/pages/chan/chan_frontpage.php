58b5bd20650af39666a7aace813a90aabe0b5679
shinmera <shinmera@tymoon.eu>
2012-06-29 22:17:19 +0200

    Finished frontpage. Various fixes in header and footer.

---
+++ data/pages/chan/chan_frontpage.php
@@ -0,0 +1,46 @@
+<? global $l,$t,$c;$t = $l->loadModule('Themes');$t->loadTheme('chan'); ?>
+<? $t->openPage('Stevenchan'); ?>
+    
+<h1 id="chanTitle">Stevenchan</h1>
+<div id="news">
+    <? $boxes = DataModel::getData('','SELECT * FROM ch_frontpage');
+    Toolkit::assureArray($boxes);
+    foreach($boxes as $box){ ?>
+    <div class="box <?=implode(' ',$box->classes)?>">
+        <h2><?=$box->title?></h2>
+        <?=$l->triggerPARSE('Purplish',$box->text);?>
+    </div>
+    <? } ?>
+    <div class="stretch"></div>
+</div>
+<div id="postSidebar">
+    <h3>Latest Posts:</h3>
+    <? $posts = DataModel::getData('','SELECT postID,folder,PID,p.subject,name,trip,file,fileOrig
+                                       FROM ch_posts AS p LEFT JOIN ch_boards ON BID=boardID
+                                       WHERE p.options NOT REGEXP ?
+                                       ORDER BY time DESC LIMIT ?',array('h',$c->o['chan_frontposts']));
+    Toolkit::assureArray($posts);
+    foreach($posts as $post){
+        if($post->PID==0)$post->PID=$post->postID; ?>
+        <div class="simplePost">
+            <div class="postInfo">
+                Post <a href="<?=PROOT.$post->folder.'/threads/'.$post->PID.'.php#'.$post->postID?>">#<?=$post->postID?></a>
+                <span class="postUsername"><?=$post->name?></span>
+                <span class="postTrip"><?=$post->trip?></span>
+            </div>
+            <article>
+                <? if($post->file!=""){ ?>
+                    <a class="postImageLink" title="<?=$post->fileOrig?>" href="<?=$c->o['chan_fileloc_extern'].$post->folder.'/files/'.$post->file?>">
+                        <img class="postImage" alt="<?=$post->fileOrig?>" src="<?=$c->o['chan_fileloc_extern'].$post->folder.'/thumbs/'.$post->file?>" border="0">
+                    </a>
+                <? } ?>
+                <blockquote>
+                    <?=$l->triggerPARSE('Purplish',substr($post->subject,0,200).'...'); ?>
+                </blockquote>
+            </article>
+            <br style="clear:both" />
+        </div>
+    <? } ?>
+</div>
+
+<? $t->closePage(); ?>
--- themes/chan/footer.php
+++ themes/chan/footer.php
@@ -1,3 +1,23 @@
+<div id="threadWatch" class="threadWatch" style="display:none;float:left;position:absolute;">
+    <table><thead>
+        <tr>
+            <th id="watchRemoveCol"></th>
+            <th id="watchBoardCol">Board</th>
+            <th id="watchIDCol">ID</th>
+            <th id="watchPosterCol">Poster</th>
+            <th id="watchTitleCol">Title</th>
+            <th id="watchUnreadCol">Unread</th>
+        </tr>
+    </thead><tbody>
+    </tbody></table>
+    <a href="#" id="watchRefreshButton" class="watchButton" title="Refresh">↻</a> 
+    <a href="#" id="watchReadButton" class="watchButton" title="Read All">✔</a> 
+    <a href="#" id="watchClearButton" class="watchButton" title="Clear">✘</a> 
+</div>
+<div id="popup" class="popup" style="display:none;">Please wait...</div>
+<div id="previewPost" style="display:none;float:left;position:absolute;"></div>
+<img id="previewImage" style="display:none;max-width:400px;max-height:400px;float:left;position:absolute;" alt="preview"/>
+
 <? global $c;
 $time = explode(' ',microtime());
 $time = $time[1]+$time[0];
