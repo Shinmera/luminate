f52f8fdea0e59c0898bfa010205bfb1d016d295a
shinmera <shinmera@tymoon.eu>
2012-06-29 22:38:16 +0200

    Fixed idiotic name failure. (500->403)

--- data/pages/chan/chan_403.php
+++ data/pages/chan/chan_403.php
@@ -4,21 +4,21 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="utf-8" />
-    <title>500, Permission Denied!</title>
+    <title>403, Permission Denied!</title>
     <link rel="icon" type="image/png" href="<?=DATAPATH?>images/500.png" />
     <? include(PAGEPATH.'/meta.php'); ?>
     <link rel='stylesheet' type='text/css' href='<?=DATAPATH?>css/chanspecial.css' />
 </head>
 <? if(BUFFER)ob_flush();flush();
-$dir = opendir(ROOT.IMAGEPATH.'chan/500/');$images = array();
+$dir = opendir(ROOT.IMAGEPATH.'chan/403/');$images = array();
 while(($file=readdir($dir))!==FALSE){
     if($file!='.'&&$file!='..')
-        $images[]=IMAGEPATH.'chan/500/'.$file;
+        $images[]=IMAGEPATH.'chan/403/'.$file;
 }closedir($dir);
 ?>
 <body>
     <img src="<?=$images[mt_rand(0,count($images)-1)]?>" alt=" " class="header" />
-    <h1><?=$c->o['chan_title']?> E500 - Permission denied.</h1>
+    <h1><?=$c->o['chan_title']?> E403 - Permission denied.</h1>
     <div id="content">
         <a id="return" href="<?=PROOT?>" title="Return to the front page">Return</a>
         <article>
