fe0c36461ae84407a02c5b2d5d98f814d2373dc7
shinmera <shinmera@tymoon.eu>
2012-07-03 22:47:41 +0200

    Added redirect tag.

--- modules/purplish/postgen.php
+++ modules/purplish/postgen.php
@@ -19,6 +19,7 @@ class PostGenerator{
         $type = '';
         if(strpos($post->options,'s')!==FALSE)$type.="sticky";
         if(strpos($post->options,'l')!==FALSE)$type.="locked";
+        if(strpos($post->options,'r')!==FALSE)$type ="redirect";
 
         flush();
         ob_start();
