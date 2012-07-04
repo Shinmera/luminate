17cbab9ebc63b58a5e233dfefcf58704fb73cc83
shinmera <shinmera@tymoon.eu>
2012-07-04 16:01:05 +0200

    Fixed footer and added stats counter.

--- themes/chan/footer.php
+++ themes/chan/footer.php
@@ -10,9 +10,10 @@ $total_time = round(($time-STARTTIME),4);
 ?>
 </div>
 <div class="footer">
+Running TyNET-<?=CORE::$version?> | Purplish-<?=Purplish::$version?><br />
 &copy;2010-<?=date("Y")?> TymoonNET/NexT, All rights reserved.<br />
-Page generated in <?=$total_time?> seconds.<br />
-Running TyNET v<?=VERSION?>.
+Page generated in <?=$total_time?> seconds.
 </div>
 </body>
 </html>
+<? global $c;$c->query("INSERT INTO ch_hits VALUES(?,?,?,?)",array($_SERVER["REMOTE_ADDR"],time(),0,0)); ?>
