a757695609492124085c04ca5b782d3203e7d3bb
shinmera <shinmera@tymoon.eu>
2012-07-04 12:38:14 +0200

    Fixed a couple of things with the stats.

--- modules/gui/Statistics.php
+++ modules/gui/Statistics.php
@@ -20,40 +20,43 @@ class Chart{
     function setSize($width,$height){$this->width=$width;$this->height=$height;}
     
     function display($dir='x'){
-        ?><table id=<?=$this->id?> style="display:none;">
-            <thead>
-                <tr>
-                    <td></td>
-                    <? foreach($this->cats as $cat){ ?>
-                        <th scope="col"><?=$cat?></th>
-                    <? } ?>
-                </tr>
-            </thead>
-            <tbody>
-                <? foreach($this->data as $name=>$dat){ 
-                    echo('<tr><th scope="row">'.$name.'</th>');
-                    foreach($this->cats as $cat){
-                        if(is_object($dat))echo('<td>'.$dat->$cat.'</td>');
-                        else               echo('<td>'.$dat[$cat].'</td>');
-                    }
-                    echo('</tr>');
-                } ?>
-            </tbody>
-        </table>
-        
-        <link rel="stylesheet" type="text/css" href="<?=DATAPATH?>css/visualize/basic.css"/>
-        <link rel="stylesheet" type="text/css" href="<?=DATAPATH?>css/visualize/visualize.css"/>
-        <script type="text/javascript" src="<?=DATAPATH?>js/visualize/excanvas.js"></script>
-        <script type="text/javascript" src="<?=DATAPATH?>js/visualize/visualize.js"></script>
-        <script type="text/javascript">
-            $(function(){
-                $('#<?=$this->id?>').visualize({'type':'<?=$this->type?>',
-                                                'title':'<?=$this->caption?>',
-                                                'parseDirection':'<?=$dir?>',
-                                                'width':'<?=$this->width?>',
-                                                'height':'<?=$this->height?>'});
-            });
-        </script><?
+        ?>
+        <blockquote>
+            <table id=<?=$this->id?> style="display:none;">
+                <thead>
+                    <tr>
+                        <td></td>
+                        <? foreach($this->cats as $cat){ ?>
+                            <th scope="col"><?=$cat?></th>
+                        <? } ?>
+                    </tr>
+                </thead>
+                <tbody>
+                    <? foreach($this->data as $name=>$dat){ 
+                        echo('<tr><th scope="row">'.$name.'</th>');
+                        foreach($this->cats as $cat){
+                            if(is_object($dat))echo('<td>'.$dat->$cat.'</td>');
+                            else               echo('<td>'.$dat[$cat].'</td>');
+                        }
+                        echo('</tr>');
+                    } ?>
+                </tbody>
+            </table>
+            <link rel="stylesheet" type="text/css" href="<?=DATAPATH?>css/visualize/basic.css"/>
+            <link rel="stylesheet" type="text/css" href="<?=DATAPATH?>css/visualize/visualize.css"/>
+            <script type="text/javascript" src="<?=DATAPATH?>js/visualize/excanvas.js"></script>
+            <script type="text/javascript" src="<?=DATAPATH?>js/visualize/visualize.js"></script>
+            <script type="text/javascript">
+                $(function(){
+                    $('#<?=$this->id?>').visualize({'type':'<?=$this->type?>',
+                                                    'title':'<?=$this->caption?>',
+                                                    'parseDirection':'<?=$dir?>',
+                                                    'width':'<?=$this->width?>',
+                                                    'height':'<?=$this->height?>'});
+                });
+            </script>
+            <br />
+        </blockquote><?
     }
 }
 ?>
