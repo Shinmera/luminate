90348d155c1fc01d2ca38b0b32b5620586db6fd6
shinmera <shinmera@tymoon.eu>
2012-07-07 15:28:02 +0200

    Fixed remove function. Added Object.size function.

--- data/js/js.js
+++ data/js/js.js
@@ -58,16 +58,24 @@ function implode (glue, pieces) {
     return pieces;
 }
 
-Array.prototype.remove = function(){
+function removeA(arr){
     var what, a= arguments, L= a.length, ax;
-    while(L && this.length){
+    while(L> 1 && arr.length){
         what= a[--L];
-        while((ax= this.indexOf(what))!= -1){
-            this.splice(ax, 1);
+        while((ax= arr.indexOf(what))!= -1){
+            arr.splice(ax, 1);
         }
     }
-    return this;
+    return arr;
 }
+
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
 
 $(document).ready(function(){
     //$(".date").each(function(){
