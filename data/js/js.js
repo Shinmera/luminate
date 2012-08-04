c84cdf02688e0dffa5b0439b22fc099e709fa41a
shinmera <shinmera@tymoon.eu>
2012-08-04 14:05:13 +0200

    Added isMobile detection variable.

--- data/js/js.js
+++ data/js/js.js
@@ -13,6 +13,24 @@ function isNumeric(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i) ? true : false;
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i) ? true : false;
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i) ? true : false;
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i) ? true : false;
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());
+    }
+};
+
 function stringContains(a,search){
     return (a.indexOf(search) != -1);
 }
