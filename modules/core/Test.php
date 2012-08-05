53672e8b55386bcaa665d3fe46204f27996e946f
shinmera <shinmera@tymoon.eu>
2012-08-05 16:45:13 +0200

    Further auto break lines fix.

--- modules/core/Test.php
+++ modules/core/Test.php
@@ -8,10 +8,9 @@ public static $hooks=array("foo");
 
 function runTests(){
     global $c,$l,$k,$t;
-    $t->openPage('A');
-    
-    Toolkit::interactiveList('B', array('Nope'), array('WELL!'), array('Nope'));
-    $t->closePage();
+    echo(nl2br(Toolkit::autoBreakLines('Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consequat molestie orci eu hendrerit. In rhoncus ornare urna a consectetur. Fusce mi est, vulputate et adipiscing condimentum, pretium eu ligula. Sed a arcu sed orci luctus mollis. Pellentesque tellus ligula, viverra id mattis a, rhoncus in tortor. Quisque egestas aliquet bibendum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean aliquam, felis eget dignissim vestibulum, quam enim hendrerit mi, ac suscipit quam nisl in mauris. Donec erat augue, pellentesque sed blandit convallis, semper vel augue. Nam augue purus, consequat at eleifend quis, ultrices quis odio.
+
+Sed vulputate, elit a tincidunt rutrum, quam tortor tristique est, at tincidunt diam orci at metus. Morbi non turpis mi, sed congue sem. Aenean pretium laoreet orci, sit amet dictum nisl vestibulum a. Aenean non nisl nibh, nec placerat orci. Aenean justo justo, aliquet at venenatis id, mattis hendrerit mauris. Nullam non dolor tellus, ut sagittis metus. Donec vel urna non odio condimentum pulvinar. Suspendisse potenti. Donec in lorem eu enim faucibus egestas sed sed mauris. Nullam adipiscing turpis et nisi elementum tempor.',120)));
 }
 
 
