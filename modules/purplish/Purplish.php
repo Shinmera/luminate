07e16a4d78b6e238dbf6756f71e9fcc44cc44c27
Shinmera <shinmera@tymoon.eu>
2012-09-07 07:50:09 +0200

    Derp.

--- modules/purplish/Purplish.php
+++ modules/purplish/Purplish.php
@@ -19,8 +19,8 @@ function displayPage(){
     if($c->o['chan_online']=='1'||$a->check('chan.admin.*')){
         switch(trim($params[0])){
             case 'byID':
-                $board = DataModel::getData('',"SELECT boardID,folder FROM ch_boards WHERE boardID=? OR folder LIKE ? LIMIT 1",array($params[1],$params[1]));
-                $thread = DataModel::getData('',"SELECT PID FROM ch_posts WHERE postID=? AND boardID=? LIMIT 1",array($params[2],$board->boardID));
+                $board = DataModel::getData('',"SELECT BID,folder FROM ch_boards WHERE boardID=? OR folder LIKE ? LIMIT 1",array($params[1],$params[1]));
+                $thread = DataModel::getData('',"SELECT PID FROM ch_posts WHERE postID=? AND boardID=? LIMIT 1",array($params[2],$board->BID));
 
                 if($board==null||$thread==null)die();
                 if($thread->PID==0)$thread->PID=$params[2];
