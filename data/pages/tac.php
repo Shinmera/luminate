db4ed16433fb6c3d1fe28e1bb2edf097bc048539
root <root@tymoon.eu>
2012-09-30 00:35:41 +0200

    Fixed security leaks in chan pages. Added a couple of files from the online repo. Fixed the broken shitbrowser page.

---
+++ data/pages/tac.php
@@ -0,0 +1,67 @@
+<h3 class="title">TERMS AND CONDITIONS</h3>
+<blockquote>
+<br />Term definition: "We" or the "Staff" are the owners, administrators and moderators of this site. "You" refers to the user or reader of this document.
+<br />In order to use this site and post content on it, you have to agree to certain conditions, for both our and your safety, as well as fair usage and treatment of users.
+<br />Registering an account or posting data promises that you have read, understood and agreed upon the terms and conditions about the usage of this site, as described in this document.
+<br />A <a href="#short">short conclusion</a> in a more relaxed writing tone is located at the bottom of this document.
+<br />
+<br />
+<b>&ensp;&sect;1: Content</b><ol>
+    <li>Any kind of content (written or otherwise) that is posted on this site has to underly and obey the laws of Switzerland and Germany.</li>
+    <li>Content that breaks &sect;1.1 can be removed at any time, even if this action overrides other rules to do so.</li>
+    <li>The content has to abide the rules of the section it is posted in or it may be removed from the site or modified.</li>
+    <li>If the section in question does not provide any specific rules, the content still has to abide the global site rules (&sect;2).</li>
+    <li>We reserve all rights to manipulate or delete content that does not follow site guidelines or rules, to ensure fair usage.</li>
+    <li>Posted content is copyright to the original poster, who may remove or modify said content himself or request a staff member to do so at any time.</li>
+    <li>&sect;1.6 may be put out of effect for certain sections of the site. This has to be specifically mentioned in the section's rules.</li>
+    <li>Rule overrides have to be either directly written or approved by an owner or administrator of the site in order to be valid.</li>
+    <li>If you find content that does not abide by the rules and obligations, please report it to the Staff via <a href="mailto:admin@tymoon.eu">e-mail</a>.</li>
+</ol>
+<b>&ensp;&sect;2: Global Site Rules</b><ol>
+    <li>All visual media (images,videos) and links posted directly on this site or linked to from here are not allowed to include any<ol type="a">
+        <li>Pornographic content</li>
+        <li>Explicit violence</li>
+        <li>Gore or Excrements</li>
+        <li>Otherwise shocking content</li>
+    </ol></li>
+    <li>Tasteful nudity is allowed, as long as the image is either in a section or thread marked as NSFW (Not Safe For Work) or is linked to with a NSFW warning.</li>
+    <li>Generally, tasteful nudity are pictures in which the nude parts are not immediate subject of the picture, or rather, the picture is not considered porn.</li>
+    <li>Whether or whether not a picture is determined as tasteful nudity is up to the Staff.</li>
+    <li>We reserve the right to intervene and punish (&sect;3.1) users if they go out of their way to directly offend others without any sign of remorse.</li>
+    <li>Any kind of harsh punishment is usually frowned upon and only used as a final retort. Infractions may occur more frequently though as they don't carry much weight and provide a meaningful guideline.</li>
+</ol>
+<b>&ensp;&sect;3: Punishment</b><ol>
+    <li>Punishment occurs in five different forms:<ol>
+        <li>Infraction: An infraction does not limit the user in any way, but serves as a reminder to the user to change his behavior. Have enough infractions been gathered by a user in a short time frame, another punishment method may be executed.</li>
+        <li>Permission removal: A users permission to post on certain sections or carry out certain actions may be restricted. The time period of this restriction may be endless or limited.</li>
+        <li>Silent ban: This ban does not directly restrict any permissions. However, only he will be able to see what he posted during the period of the ban, making him appear mute to the outside.</li>
+        <li>Account ban/IP ban: This simply blocks the user from posting any content or accessing his account. The time period of the ban may be endless or limited.</li>
+        <li>Account deletion: This is the final option and includes deleting all data the user has ever contributed to the site. This ban cannot be reverted.</li>
+    </ol></li>
+    <li>An appeal to punishments of type 2 or 3 may be discussed with the Staff.</li>
+    <li>We reserve the right to reject any appeal.</li>
+    <li>Joke bans (bans with a time period of less than a minute) do not influence the banned user's overall score.
+</ol>
+<b>&ensp;&sect;4: Staff</b><ol>
+    <li>The Staff consists of four different levels of authority:<ol>
+        <li>Moderator: Moderators usually have the task to look over the users and keep everything in order in a certain section.</li>
+        <li>Super-Moderator: The Super-Moderators are site-wide moderators.</li>
+        <li>Administrator: Administrators have the additional task of keeping the site up and making sure everything works. They may not be very active as moderators though.</li>
+        <li>Root: The site owner has total control over everything, but has the obligation to make sure that the Staff stays well organized and that the server runs smoothly. Additionally, he has the task of actually programming TyNET and expand it for new sections.</li>
+    </ol></li>
+    <li>The people are hand picked by the site owner into the Staff.</li>
+    <li>If the reliability of a Staff member is being questioned, please contact the site owner via email or PM.</li>
+    <li>Staff posts do not have any more weight than a regular users post, unless the matter of discussion directly concerns the Staff member's tasks.</li>
+    <li>Staff members are generally just seen as normal users with more obligations. This is to ensure equality for all users.</li>
+</ol>
+<br />
+<br /><a name="short" style="font-weight:bold;">This is all way too boring and long for me to read. Give me a short version!</a>
+<br />Alright. Basically, as soon as you register an account on here or post content, you agree to what's written in this document.
+<br />The things you post have to abide by Swiss and German laws and are not allowed to include any kind of pornographic, gore or violent content. This is simply to ensure a certain standard of content (If you really want such things, there's enough sites on the web for it already).
+<br />You are allowed to be rude to some point and people should rather learn to tolerate things instead of whining about them, but if it goes out of hand, the Staff can still ban you.
+<br />
+<br /><a name="shorter" style="font-weight:bold;">That was still tl;dr!</a>
+<br />Ok, even shorter: Don't be too much of an ass, abide the laws. Don't post disgusting shit.
+</blockquote>
+<br />
+<br /><span style="font-size:10pt;font-style:italic;">Last Modified on 17.8.2012 by Shinmera</span>
