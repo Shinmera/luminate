be0da57ce50219b7fc73d69f0db46b132e912441
shinmera <shinmera@tymoon.eu>
2012-10-05 18:32:25 +0200

    Minor.

---
+++ modules/ticker/Ticker.php
@@ -0,0 +1,11 @@
+<? class Ticker{
+public static $name="Ticker";
+public static $author="NexT";
+public static $version=0.01;
+public static $short='ticker';
+public static $required=array("Auth","Themes");
+public static $hooks=array("foo");
+
+
+}
+?>
