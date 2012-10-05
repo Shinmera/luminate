f80b96e48d5c7c5a3be38d5397f8b14855762b36
shinmera <shinmera@tymoon.eu>
2012-10-05 18:32:25 +0200

    Minor.

---
+++ modules/hub/Hub.php
@@ -0,0 +1,11 @@
+<? class Hub{
+public static $name="Module";
+public static $author="NexT";
+public static $version=0.01;
+public static $short='hub';
+public static $required=array("Auth","Themes");
+public static $hooks=array("foo");
+
+
+}
+?>
