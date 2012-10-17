285f1b363eaa51df05fefbfa223ab740a4343cd7
shinmera <shinmera@tymoon.eu>
2012-10-17 13:29:05 +0200

    Fixed board page generation.

--- modules/purplish/boardgen.php
+++ modules/purplish/boardgen.php
@@ -14,79 +14,84 @@ class BoardGenerator{
         $t = $l->loadModule('Themes');
         $t->loadTheme("chan");
         $PAGETITLE=$board->title.' - '.$c->o['chan_title'];
-        
-        $totalthreads = $c->getData("SELECT COUNT(postID) FROM ch_posts WHERE BID=? AND PID=0 AND options NOT REGEXP ?",array($board->boardID,'d'));
-        $totalthreads = $totalthreads[0]['COUNT(postID)'];
-        $threads=array(1);
+
+        $stickies= DataModel::getData('ch_posts',"SELECT postID FROM ch_posts WHERE BID=? AND PID=0 AND options NOT REGEXP ? AND options REGEXP ? ORDER BY bumptime DESC",
+                                                    array($board->boardID,'d','s'));
+        Toolkit::assureArray($stickies);
+        $threads = DataModel::getData('ch_posts','SELECT postID FROM ch_posts WHERE BID=? AND PID=0 AND options NOT REGEXP ? AND options NOT REGEXP ? ORDER BY bumptime DESC LIMIT ?,?',
+                                                    array($board->boardID,'d','s',0,($c->o['chan_tpp']*$board->maxpages)-count($stickies)));
+        Toolkit::assureArray($threads);
+        $threads = array_merge($stickies, $threads);
         
         $NO_BUFFER=true; //To stop the theme header to flush automatically
         ob_start();
-        for($i=0;count($threads)>0;$i++){
-            if($i>$board->maxpages){
-                if(!class_exists("DataGenerator"))include(TROOT.'modules/chan/datagen.php');
-                $threads = DataModel::getData('ch_posts',"SELECT postID FROM ch_posts WHERE BID=? AND PID=0 AND options NOT REGEXP ? ORDER BY bumptime DESC LIMIT ".
-                        ($i*$c->o['chan_tpp']).",".(($i+1)*$c->o['chan_tpp']),array($board->boardID,'d'));
-                Toolkit::assureArray($threads);
-                $datagen = new DataGenerator();
-                for($j=0;$j<count($threads);$j++)$datagen->deletePost($threads[$j]->postID, $board->boardID, false, false);
-            }else{
-                ob_clean();
-                $threads = DataModel::getData('ch_posts',"SELECT postID,BID FROM ch_posts WHERE BID=? AND PID=0 AND options NOT REGEXP ? AND options REGEXP ? ORDER BY bumptime DESC LIMIT ".
-                        ($i*$c->o['chan_tpp']).",".(($i+1)*$c->o['chan_tpp']),array($board->boardID,'d','s'));
-                $Uthreads= DataModel::getData('ch_posts',"SELECT postID,BID FROM ch_posts WHERE BID=? AND PID=0 AND options NOT REGEXP ? AND options NOT REGEXP ? ORDER BY bumptime DESC LIMIT ".
-                        ($i*$c->o['chan_tpp']).",".(($i+1)*$c->o['chan_tpp']),array($board->boardID,'d','s'));
-                Toolkit::assureArray($threads);
-                Toolkit::assureArray($Uthreads);
-                $threads=array_merge($threads,$Uthreads);
-                ?>
 
-                <?='<? define("POST_SHORT",TRUE); ?>'?>
-                <? require_once(PAGEPATH.'chan/chan_header.php'); ?>
-                <?=write_header($board->title.' - '.$c->o['chan_title'],$board,0,$board->options)?>
+        //Build board pages
+        $totalthreads = count($threads);
+        for($i=0;$i<$totalthreads;$i+=$c->o['chan_tpp']){
+            ?>
+            <?='<? define("POST_SHORT",TRUE); ?>'?>
+            <? require_once(PAGEPATH.'chan/chan_header.php'); ?>
+            <?=write_header($board->title.' - '.$c->o['chan_title'],$board,0,$board->options)?>
 
-                <input type="hidden" id="view" value="board" />
-                <div class="board">
-                    <? for($j=0;$j<count($threads);$j++){
-                        if($genposts||$genthreads)ThreadGenerator::generateThread($threads[$j]->postID, $threads[$j]->BID,true);
-                        $posts = DataModel::getData('ch_posts',"SELECT postID FROM ch_posts WHERE BID=? AND PID=? AND options NOT REGEXP ? ORDER BY postID DESC LIMIT 3",array($threads[$j]->BID,$threads[$j]->postID,'d'));
-                        $postcount = $c->getData("SELECT COUNT(postID) from ch_posts WHERE BID=? AND PID=? AND options NOT REGEXP ?",array($threads[$j]->BID,$threads[$j]->postID,'d'));$postcount=$postcount[0]['COUNT(postID)'];
-                        Toolkit::assureArray($posts);
-                        
-                        $folder=$board->folder;
-                        ?><div class='threadToolbar'>
-                            <a href='<?=PROOT.$folder.'/threads/'.$threads[$j]->postID.'.php?b=1'?>'>Whole Thread(<?=$postcount?>)</a>
-                            <a href='<?=PROOT.$folder.'/threads/'.$threads[$j]->postID.'.php?b=-50'?>'>Last 50</a> 
-                            <a href='<?=PROOT.$folder.'/threads/'.$threads[$j]->postID.'.php?e=100'?>'>First 100</a> 
-                            <a href='#' class='watchThread' id='<?=$threads[$j]->postID?>'>Watch</a> 
-                            <a href='#' class='hideThread' id='<?=$threads[$j]->postID?>'>Hide</a>
-                        </div>
-                        
-                        <?='<? @ include("'.ROOT.DATAPATH.'chan/'.$folder.'/posts/'.$threads[$j]->postID.'.php"); ?>'?>
-                        <a class="omittedText" href="<?=PROOT.$folder.'/threads/'.$threads[$j]->postID.'.php'?>">
-                            (<?=$postcount-count($posts)?> posts omitted.)
-                        </a>
-                        <div class='thread' id='T<?=$threads[$j]->postID?>' >
-                            <? for($n=count($posts)-1;$n>=0;$n--){ ?>
-                                <?='<? @ include("'.ROOT.DATAPATH.'chan/'.$folder.'/posts/'.$posts[$n]->postID.'.php"); ?>'."\n"?>
-                            <? } ?>
-                        </div>
-                        <br class='clear' />
-                    <? } ?>
-                </div>
-                <br class="clear" />
-                <?=$k->pager(PROOT.$board->folder.'/',$totalthreads,$i,$c->o['chan_tpp'],true)."\n"?>
-                <? require_once(PAGEPATH.'chan/chan_footer.php'); ?>
-                <?=write_footer($board->title.' - '.$c->o['chan_title'],$board->boardID,$board->folder,0,$board->options);?>
+            <div id="view" class="board">
+                <? for($j=$i;$j<$i+$c->o['chan_tpp'] && $j<$totalthreads;$j++){
+                    if($genposts||$genthreads)ThreadGenerator::generateThread($threads[$j]->postID, $board->boardID,true);
+                    $posts = DataModel::getData('ch_posts',"SELECT postID FROM ch_posts WHERE BID=? AND PID=? AND options NOT REGEXP ? ORDER BY postID DESC LIMIT 3",
+                                                            array($board->boardID,$threads[$j]->postID,'d'));
+                    $postcount = $c->getData("SELECT COUNT(postID) from ch_posts WHERE BID=? AND PID=? AND options NOT REGEXP ?",
+                                                            array($board->boardID,$threads[$j]->postID,'d'));
+                    $postcount=$postcount[0]['COUNT(postID)'];
+                    Toolkit::assureArray($posts);
+                    
+                    $folder=$board->folder;
+                    ?><div class='threadToolbar'>
+                        <a href='<?=PROOT.$folder.'/threads/'.$threads[$j]->postID.'.php?b=1'?>'>Whole Thread(<?=$postcount?>)</a>
+                        <a href='<?=PROOT.$folder.'/threads/'.$threads[$j]->postID.'.php?b=-50'?>'>Last 50</a> 
+                        <a href='<?=PROOT.$folder.'/threads/'.$threads[$j]->postID.'.php?e=100'?>'>First 100</a> 
+                        <a href='#' class='watchThread' id='<?=$threads[$j]->postID?>'>Watch</a> 
+                        <a href='#' class='hideThread'  id='<?=$threads[$j]->postID?>'>Hide</a>
+                    </div>
+                    
+                    <?='<? @ include("'.ROOT.DATAPATH.'chan/'.$folder.'/posts/'.$threads[$j]->postID.'.php"); ?>'?>
+                    <a class="omittedText" href="<?=PROOT.$folder.'/threads/'.$threads[$j]->postID.'.php'?>">
+                        (<?=$postcount-count($posts)?> posts omitted.)
+                    </a>
+                    <div class='thread' id='T<?=$threads[$j]->postID?>' >
+                        <?='<?'?>
+                        <? for($n=count($posts)-1;$n>=0;$n--){ ?>
+                            <?=' @ include("'.ROOT.DATAPATH.'chan/'.$folder.'/posts/'.$posts[$n]->postID.'.php");'."\n"?>
+                        <? } ?>
+                        <?='?>'?>
+                    </div>
+                    <br class='clear' />
+                <? } ?>
+            </div>
+            <br class="clear" />
+            <?=$k->pager(PROOT.$board->folder.'/',$totalthreads,$i,$c->o['chan_tpp'],true)."\n"?>
+            <? require_once(PAGEPATH.'chan/chan_footer.php'); ?>
+            <?=write_footer($board->title.' - '.$c->o['chan_title'],$board->boardID,$board->folder,0,$board->options);?>
 
-                <?                    
-                file_put_contents($path.$i.'.php',ob_get_contents(),LOCK_EX);
-                ob_clean();
-            }
+            <?                    
+            file_put_contents($path.$i.'.php',ob_get_contents(),LOCK_EX);
+            ob_clean();
         }
+
+        //Delete posts.
+        $toDelete = DataModel::getData("ch_posts","SELECT postID FROM ch_posts WHERE BID=? AND TID=0 AND options NOT REGEXP ? AND options NOT REGEXP ? ORDER BY bumptime DESC LIMIT ?,?",
+                                                    array($board->boardID,'d','s',$totalthreads,18446744073709551615));
+        Toolkit::assureArray($toDelete);
+        $datagen = new DataGenerator();
+        foreach($toDelete as $thread){
+            $datagen->deletePost($thread->postID, $board->boardID, false, false);
+        }
+
         file_put_contents($path.'index.php','<?php include("'.ROOT.DATAPATH.'chan/'.$board->folder.'/0.php"); ?>',LOCK_EX);
         ob_end_clean();
         $NO_BUFFER=false;
         $t->loadTheme($previousTheme);
+
+        //That's it.
     }
 }
 ?>
