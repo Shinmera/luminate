97cc4a9430d3380044a0ae5f062a4996e6b38a00
shinmera <shinmera@tymoon.eu>
2012-11-03 12:03:16 +0100

    test page to convert everything into unicode.

--- modules/core/Test.php
+++ modules/core/Test.php
@@ -9,8 +9,17 @@ public static $hooks=array("foo");
 function runTests(){
     global $c,$l,$k,$t;
 
-    
-    
+    $tables = $c->getData("SHOW TABLES");
+    foreach($tables as $table){
+        foreach($table as $b=>$t){
+            echo("CONVERTING: ".$t."<br />");
+            $c->query("ALTER TABLE " . $t . " CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci",array());
+            //$cols = $c->getData("SHOW COLUMNS FROM ".$t);
+            //foreach($cols as $col){
+            //    echo("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CONVERTING: ".$col['Field']."<br />");
+            //    $c->query("ALTER TABLE " . $t . " CHANGE " . $col['Field'] . " CHARACTER SET utf8 COLLATE utf8_unicode_ci",array());
+            //}
+        }
+    }
 }
-}
-?>
+}?>
