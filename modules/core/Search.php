579ebe087735d140853a668d24b5c00a2d52dc71
shinmera <shinmera@tymoon.eu>
2012-11-17 21:39:34 +0100

    Begin of search module.

---
+++ modules/core/Search.php
@@ -0,0 +1,17 @@
+<? class Search{
+public static $name="Search";
+public static $author="NexT";
+public static $version=0.01;
+public static $short='search';
+public static $required=array("Auth");
+public static $hooks=array("foo");
+
+function setDatabaseStructure($fields){
+    
+}
+
+function search($query){
+
+}
+}
+?>
