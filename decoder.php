9a3fd9a663b69f395840224660e3d5edfccac091
shinmera <shinmera@tymoon.eu>
2012-12-09 13:58:32 +0100

    Added redirect on _ special directories.

--- decoder.php
+++ decoder.php
@@ -5,25 +5,42 @@ try{
     if(BUFFER){if(COMPRESS){if(!ob_start('ob_gzhandler')) ob_start();}}
     Toolkit::checkShitBrowser();
 
-    //PARSE URL
-    if(PROOT!=='/')$param=str_replace(PROOT,'',$_SERVER['REQUEST_URI']);
-    else           $param=substr($_SERVER['REQUEST_URI'],1);
+    //Strip PROOT if included.
+    if(strpos($_SERVER['REQUEST_URI'], PROOT) === 0)$param=substr($_SERVER['REQUEST_URI'], strlen(PROOT));
+    else                                            $param=substr($_SERVER['REQUEST_URI'],1);
 
-    $param=urldecode($param);
+    //Decode parameters (directory path)
+    $param=trim(urldecode($param));
     if(strpos($param,'?')!==FALSE)$param=substr($param,0,strpos($param,'?'));
-    $param=trim($param);
     $params=explode('/',$param);
-    if($params[0]==PROOT)$params=array_slice($params, 1);
+
+    //Determine active subdomain
     $domain=str_replace(HOST,'',$_SERVER['HTTP_HOST']);
     if($domain!='')$domain=strtolower(substr($domain,0,strlen($domain)-1));
     
-    if($params[0]=='api'){ //API hack
+    //Api special hack to avoid the AJAX domain issue.
+    if($params[0]=='api' || $params[0]=='_api'){ //API hack
         $domain='api';
+        $param=substr($param, strlen($params[0])+1);
         $params=array_slice($params, 1);
     }
+    //Redirect on special underscore directory:
+    if(substr($params[0], 0, 1) == '_'){
+        $domain=substr($params[0], 1);
+        $param=substr($param, strlen($params[0])+1);
+        header('Location: http://'.$domain.'.'.HOST.PROOT.$param);
+        $c->close();
+        ob_end_flush();
+        flush();
+        die();
+    }
+
+    //Default to index if no subdomain
     if($params[0]=='')$site='index';else $site=$params[0];
+    //Override to offline if in offline mode.
     if($c->o['offline']==='1'&&$domain!='admin'&&
        strpos(TRUSTEDIPS,$_SERVER['REMOTE_ADDR'])===FALSE)$domain="offline";
+
     define('DOMAIN',$domain);
     define('FULLURL',$domain.'.'.HOST.$_SERVER['REQUEST_URI']);
     define('PARAM',$param);
@@ -34,10 +51,10 @@ try{
     if($DOMINATINGMODULE=='')
         $l->triggerHook('HITDOMAIN','CORE',array($params),array(),true);
 
+    //Cleanup
     if(BUFFER)ob_end_flush();
     flush();
     $c->close();
-
     $l->triggerHook('END',$CORE,array($k->getMicrotime()));
 
 }catch(Exception $e){
